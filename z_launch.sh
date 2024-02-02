dc7b6ab017152c788eec70833a7fe3b4ae220e55
ywateba <yao.watebaa@gmail.com>
2024-02-02 02:41:31 +0100

    updates

--- z_launch.sh
+++
@@ -1,4 +0,0 @@
-kubectl apply -f deployments/secrets/databse-secret..yaml
-kubectl apply -f deployments/analytics/db-configmap.yaml
-sleep 10
-kubectl apply -f deployments/analytics/analytics-api.yaml
