121bab82b7fd75dfa526f04eceb2aabdd99c1e31
ywateba <yao.watebaa@gmail.com>
2024-02-04 16:05:28 +0100

    updates

--- analytics/config.py
+++ analytics/config.py
@@ -1,19 +1,34 @@
 import logging
 import os
+import boto3
 import watchtower
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# Create a boto3 session with your specified region
+client = boto3.session.Session(
+    region_name='us-east-1',
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+).client("logs") # Replace 'us-west-2' with your desired region
+
+
+# # Configure logging
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+                    force=True)
+
+logger = logging.getLogger("app")
 handler = watchtower.CloudWatchLogHandler(
     log_group="uda-app-group",
-    stream_name="analytics"
+    stream_name="analytics",
+
+    boto3_client=client
+
+
 )
 logger.addHandler(handler)
+
 
 
 
@@ -24,8 +39,9 @@ db_port = os.environ.get("DB_PORT", "5432")
 db_name = os.environ.get("DB_NAME", "postgres")
 
 app = Flask(__name__)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 db = SQLAlchemy(app)
+
 app.logger = logger
-app.logger.setLevel(logging.DEBUG)
